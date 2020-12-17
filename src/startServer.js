ab67d0f90ef8da8ab099f62be85c2e80725a9816
Matheus0liveira <matheusfilho98.ms@gmail.com>
2020-12-17 10:36:46 -0300

    feat: add connection with mongodb

--- src/startServer.js
+++ src/startServer.js
@@ -1,6 +1,16 @@
 const { ApolloServer } =  require('apollo-server');
 
+const mongoose = require('mongoose');
+
+
+
 const startServer = ({typeDefs, resolvers}) => {
+
+  mongoose.connect('mongodb://localhost:27017/graphql', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }).then(() => console.log('🏃 Mongo started'));
+
 
   const server = new ApolloServer({typeDefs, resolvers});
 
